ff26048f0366f85672a9af62420a86a3d4189651
Synthird <105394020+Synthird@users.noreply.github.com>
2024-10-15 17:36:25 -0700

    Returns NaN if divided by 0 except for sec(0)

--- src/TrigReciprocal.java
+++ src/TrigReciprocal.java
@@ -1,14 +1,14 @@
 public class TrigReciprocal {
 
     public double cosecant(double number) {
-        return 1/Math.sin(number);
+        return number == 0.0 ? Double.NaN : 1.0 / Math.sin(number);
     }
 
     public double secant(double number) {
-        return 1/Math.cos(number);
+        return 1.0 / Math.cos(number);
     }
 
     public double cotangent(double number) {
-        return 1/Math.tan(number);
+        return number == 0.0 ? Double.NaN : 1.0 / Math.tan(number);
     }
 }
