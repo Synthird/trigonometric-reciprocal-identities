747385d874d2e8adf85e38b2be2e97f05b289fc8
Synthird <105394020+Synthird@users.noreply.github.com>
2024-12-10 00:49:33 -0800

    Added javadocs above the trigReciprocal methods

--- src/TrigReciprocal.java
+++ src/TrigReciprocal.java
@@ -1,12 +1,24 @@
 public class TrigReciprocal {
+    /**
+     * @param numberInRadians
+     * @return The cosecant of a number in radians.
+     */
     public double csc(double numberInRadians) {
         return numberInRadians == 0.0 ? Double.NaN : 1.0 / Math.sin(numberInRadians);
     }
 
+    /**
+     * @param numberInRadians
+     * @return The secant of a number in radians.
+     */
     public double sec(double numberInRadians) {
         return 1.0 / Math.cos(numberInRadians);
     }
 
+    /**
+     * @param numberInRadians
+     * @return The cotangent of a number in radians.
+     */
     public double cot(double numberInRadians) {
         return numberInRadians == 0.0 ? Double.NaN : 1.0 / Math.tan(numberInRadians);
     }
