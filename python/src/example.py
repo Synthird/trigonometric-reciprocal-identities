980666f33259032f52aeb5a9f6e516e4acbdc9ba
Synthird <105394020+Synthird@users.noreply.github.com>
2024-12-28 18:37:56 -0800

    Addded decimals to whole numbers in the python example

--- python/src/example.py
+++ python/src/example.py
@@ -2,7 +2,7 @@ import trig_reciprocal
 
 # This file is an example of using the trigReciprocal methods.
 
-print(f"csc(1) = {trig_reciprocal.csc(1)} radians")
-print(f"sec(1) = {trig_reciprocal.sec(1)} radians")
-print(f"sec(0) = {trig_reciprocal.sec(0)} radians")
-print(f"cot(1) = {trig_reciprocal.cot(1)} radians")
+print(f"csc(1) = {trig_reciprocal.csc(1.0)} radians")
+print(f"sec(1) = {trig_reciprocal.sec(1.0)} radians")
+print(f"sec(0) = {trig_reciprocal.sec(0.0)} radians")
+print(f"cot(1) = {trig_reciprocal.cot(1.0)} radians")
