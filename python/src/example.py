a35a1cef2c12faeae72633441fabc4422aedda40
Synthird <105394020+Synthird@users.noreply.github.com>
2024-12-28 18:39:12 -0800

    The radian unit is singular when displaying secant of 0

--- python/src/example.py
+++ python/src/example.py
@@ -4,5 +4,5 @@ import trig_reciprocal
 
 print(f"csc(1) = {trig_reciprocal.csc(1.0)} radians")
 print(f"sec(1) = {trig_reciprocal.sec(1.0)} radians")
-print(f"sec(0) = {trig_reciprocal.sec(0.0)} radians")
+print(f"sec(0) = {trig_reciprocal.sec(0.0)} radian")
 print(f"cot(1) = {trig_reciprocal.cot(1.0)} radians")
