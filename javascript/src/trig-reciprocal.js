d179bcf196beec2e940615709e12093e6506ba58
Synthird <105394020+Synthird@users.noreply.github.com>
2025-01-17 21:31:35 -0800

    Added javaScript docstrings

--- javascript/src/trig-reciprocal.js
+++ javascript/src/trig-reciprocal.js
@@ -1,14 +1,29 @@
 let potentialNaN = 0.0;
 
+/**
+ * 
+ * @param {*} numberInRadians 
+ * @returns The cosecant of numberInRadians.
+ */
 export function csc(numberInRadians) {
 	potentialNaN = parseFloat(numberInRadians);
 	return potentialNaN === 0.0 || isNaN(potentialNaN) ? NaN : 1.0 / Math.sin(numberInRadians);
 }
 
+/**
+ * 
+ * @param {*} numberInRadians 
+ * @returns The secant of numberInRadians.
+ */
 export function sec(numberInRadians) {
 	return 1.0 / Math.cos(numberInRadians);
 }
 
+/**
+ * 
+ * @param {*} numberInRadians 
+ * @returns The cotangent of numberInRadians.
+ */
 export function cot(numberInRadians) {
 	potentialNaN = parseFloat(numberInRadians);
 	return potentialNaN === 0.0 || isNaN(potentialNaN) ? NaN : 1.0 / Math.tan(numberInRadians);
