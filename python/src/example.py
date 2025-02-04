acba704aa639ed2ab7a726e255dc50842e2f96e6
Synthird <105394020+Synthird@users.noreply.github.com>
2025-02-04 14:05:51 -0800

    Added csc(0) and cot(0) examples in the python folder

--- python/src/example.py
+++ python/src/example.py
@@ -4,5 +4,18 @@ import trig_reciprocal
 
 print(f"csc(1) = {trig_reciprocal.csc(1.0)} radians")
 print(f"sec(1) = {trig_reciprocal.sec(1.0)} radians")
-print(f"sec(0) = {trig_reciprocal.sec(0.0)} radian")
 print(f"cot(1) = {trig_reciprocal.cot(1.0)} radians")
+
+print("--- 0 as the parameter ---")
+
+print(f"sec(0) = {trig_reciprocal.sec(0.0)} radian")
+
+try:
+    trig_reciprocal.csc(0)
+except ZeroDivisionError:
+    print("csc(0) = undefined!")
+
+try:
+    trig_reciprocal.cot(0)
+except ZeroDivisionError:
+    print("cot(0) = undefined!")
