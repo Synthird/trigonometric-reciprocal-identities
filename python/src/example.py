314d4212f623cc022c12e8ef1a4e2d86e1f4300b
Synthird <105394020+Synthird@users.noreply.github.com>
2025-02-06 10:57:29 -0800

    Change the Python example file to inputing a number

--- python/src/example.py
+++ python/src/example.py
@@ -2,20 +2,8 @@ import trig_reciprocal
 
 # This file is an example of using the trig_reciprocal methods.
 
-print(f"csc(1) = {trig_reciprocal.csc(1.0)} radians")
-print(f"sec(1) = {trig_reciprocal.sec(1.0)} radians")
-print(f"cot(1) = {trig_reciprocal.cot(1.0)} radians")
+number: float = float(input("Please enter a number: "))
 
-print("--- 0 as the parameter ---")
-
-print(f"sec(0) = {trig_reciprocal.sec(0.0)} radian")
-
-try:
-    trig_reciprocal.csc(0)
-except ZeroDivisionError:
-    print("csc(0) = undefined!")
-
-try:
-    trig_reciprocal.cot(0)
-except ZeroDivisionError:
-    print("cot(0) = undefined!")
+print(f"csc(1) = {trig_reciprocal.csc(number)} radians")
+print(f"sec(1) = {trig_reciprocal.sec(number)} radians")
+print(f"cot(1) = {trig_reciprocal.cot(number)} radians")
