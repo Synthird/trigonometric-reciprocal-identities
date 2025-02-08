3037849e217541f92eb7171101f139e330942e2c
Synthird <105394020+Synthird@users.noreply.github.com>
2025-02-07 22:40:59 -0800

    Handled input errors in the Python example

--- python/src/example.py
+++ python/src/example.py
@@ -2,8 +2,18 @@ import trig_reciprocal
 
 # This file is an example of using the trig_reciprocal methods.
 
-number: float = float(input("Please enter a number: "))
+number: float = 0.0
 
-print(f"csc(1) = {trig_reciprocal.csc(number)} radians")
-print(f"sec(1) = {trig_reciprocal.sec(number)} radians")
-print(f"cot(1) = {trig_reciprocal.cot(number)} radians")
+
+try:
+	number = float(input("Please enter a number: "))
+
+	print(f"csc(1) = {trig_reciprocal.csc(number)} radians")
+	print(f"sec(1) = {trig_reciprocal.sec(number)} radians")
+	print(f"cot(1) = {trig_reciprocal.cot(number)} radians")
+except ZeroDivisionError:
+	print("csc(0) and cot(0) are undefined!")
+except ValueError:
+	print("You didn't enter a number....")
+except KeyboardInterrupt:
+	print("\nYou exited via keyboard!")
