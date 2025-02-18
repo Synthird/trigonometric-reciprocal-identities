b42bcdcf4312c09ed5d0c908ae35dd3f50609398
Synthird <105394020+Synthird@users.noreply.github.com>
2025-02-18 14:49:26 -0800

    Converted Python docstrings from reST to Numpydoc

--- python/src/trig_reciprocal.py
+++ python/src/trig_reciprocal.py
@@ -6,44 +6,64 @@ Functions:
 - csc: The reciprocal of sin.
 - sec: The reciprocal of cos.
 - cot: The reciprocal of tan.
-
-:Version: 1.0.0
-:Authors: Synthird
 """
 
 import math
 
 
 def csc(angle_in_radians: float) -> float:
-	"""
-	The reciprocal of sin.
-	
-	:param angle_in_radians: An angle in radians.
-	:type angle_in_radians: float
-	:return float: The cosecant of angle_in_radians in radians.
-	:raises ZeroDivisionError: If angle_in_radians is 0.
+	"""The reciprocal of sin.
+
+	Parameters
+	----------
+	angle_in_radians : float
+		An angle in radians.
+
+	Returns
+	-------
+	float
+		The cosecant of angle_in_radians in radians.
+
+	Raises
+	------
+	ZeroDivisionError
+		If angle_in_radians is 0.
 	"""
 	return 1.0 / math.sin(angle_in_radians)
 
 
 def sec(angle_in_radians: float) -> float:
-	"""
-	The reciprocal of cos.
+	"""The reciprocal of cos.
 
-	:param angle_in_radians: An angle in radians.
-	:type angle_in_radians: float
-	:return float: The secant of angle_in_radians in radians.
+	Parameters
+	----------
+	angle_in_radians : float
+		An angle in radians.
+
+	Returns
+	-------
+	float
+		The secant of angle_in_radians in radians.
 	"""
 	return 1.0 / math.cos(angle_in_radians)
 
 
 def cot(angle_in_radians: float) -> float:
-	"""
-	The reciprocal of tan.
+	"""The reciprocal of tan.
 
-	:param angle_in_radians: An angle in radians.
-	:type angle_in_radians: float
-	:return float: The cotangent of angle_in_radians in radians.
-	:raises ZeroDivisionError: If angle_in_radians is 0.
+	Parameters
+	----------
+	angle_in_radians : float
+		An angle in radians.
+
+	Returns
+	-------
+	float
+		The cotangent of angle_in_radians in radians.
+
+	Raises
+	------
+	ZeroDivisionError
+		If angle_in_radians is 0.
 	"""
 	return 1.0 / math.tan(angle_in_radians)
