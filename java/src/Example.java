501928af7d5ef046daf82005ced1c42e5cfe694c
Synthird <105394020+Synthird@users.noreply.github.com>
2025-04-06 18:52:18 -0700

    Revamped the Java example to be more simpler

--- java/src/Example.java
+++ java/src/Example.java
@@ -8,40 +8,12 @@ public class Example {
 	private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		printHeading("Please choose a trigonometric reciprocal identity");
-		System.out.println("1 = Cosecant (csc)");
-		System.out.println("2 = Secant (sec)");
-		System.out.println("3 = Cotangent (cot)");
+		double number = 0;
 
-		double choice = inputDouble("Chosen identity");
-		double number = inputDouble("Please enter a number into your chosen identity");
-
-		printHeading("ANSWER");
-
-		if (choice == 1.0) {
-			System.out.printf("csc(%s) = %s radians", number, TrigReciprocal.csc(number));
-		} else if (choice == 2.0) {
-			System.out.printf("sec(%s) = %s radians", number, TrigReciprocal.sec(number));
-		} else if (choice == 3.0) {
-			System.out.printf("cot(%s) = %s radians", number, TrigReciprocal.cot(number));
-		} else {
-			System.out.println("Whoops! You didn't choose a trigonometric reciprocal identity....");
-		}
-
-		scanner.close();
-	}
-
-	private static void printHeading(String title) {
-		System.out.println(String.format("--- %s ---", title));
-	}
-
-	private static double inputDouble(String message) {
-		double inputNumber = 0;
-
-		System.out.print(String.format("%s: ", message));
+		System.out.print("Please enter a number: ");
 
 		try {
-			inputNumber = scanner.nextDouble();
+			number = scanner.nextDouble();
 			scanner.nextLine();
 		} catch (InputMismatchException e) {
 			exitWithMessage("Uh oh! You entered something that isn't a number.......");
@@ -49,7 +21,11 @@ public class Example {
 			exitWithMessage("\nYou exited via keyboard!");
 		}
 
-		return inputNumber;
+		System.out.println(String.format("csc(%s) = %s radian(s)", number, TrigReciprocal.csc(number)));
+		System.out.println(String.format("sec(%s) = %s radian(s)", number, TrigReciprocal.sec(number)));
+		System.out.println(String.format("cot(%s) = %s radian(s)", number, TrigReciprocal.cot(number)));
+
+		scanner.close();
 	}
 
 	private static void exitWithMessage(String message) {
