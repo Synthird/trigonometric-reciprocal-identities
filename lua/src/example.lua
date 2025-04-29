a08a48e8a07fb56bd40f021893126f70958303cc
Synthird <105394020+Synthird@users.noreply.github.com>
2025-04-28 18:05:07 -0700

    Added inverse functions in the Lua version

--- lua/src/example.lua
+++ lua/src/example.lua
@@ -5,11 +5,17 @@ local trigreciprocal = require("trigreciprocal")
 print(string.format("csc(1) = %s", trigreciprocal.csc(1)))
 print(string.format("sec(1) = %s", trigreciprocal.sec(1)))
 print(string.format("cot(1) = %s", trigreciprocal.cot(1)))
+print(string.format("arccsc(1) = %s", trigreciprocal.acsc(1)))
+print(string.format("arcsec(1) = %s", trigreciprocal.asec(1)))
+print(string.format("arccot(1) = %s", trigreciprocal.acot(1)))
 
 print("--- 0 as the parameter ---")
 
 local cosecant_zero = pcall(trigreciprocal.csc, trigreciprocal, 0)
 local cotangent_zero = pcall(trigreciprocal.cot, trigreciprocal, 0)
+
+local arccosecant_zero = pcall(trigreciprocal.csc, trigreciprocal, 0)
+local arcsecant_zero = pcall(trigreciprocal.asec, trigreciprocal, 0)
 
 if type(cosecant_zero) ~= "number" then
 	print("csc(0) = undefined!")
@@ -19,4 +25,12 @@ print(string.format("sec(0) = %s", trigreciprocal.sec(0)))
 
 if type(cotangent_zero) ~= "number" then
 	print("cot(0) = undefined!")
+end
+
+if type(arccosecant_zero) ~= "number" then
+	print("arccsc(0) = undefined!")
+end
+
+if type(arcsecant_zero) ~= "number" then
+	print("arcsec(0) = undefined!")
 end
--- lua/src/trigreciprocal.lua
+++ lua/src/trigreciprocal.lua
@@ -8,7 +8,7 @@
 ---
 ---@module "trigreciprocal"
 ---@author Synthird
----@version 1.0.1
+---@version 1.1.0
 local trigreciprocal = {}
 
 ---The reciprocal of sin.
@@ -16,7 +16,7 @@ local trigreciprocal = {}
 ---@return number --The cosecant of angle_in_radians in radians.
 function trigreciprocal.csc(angle_in_radians)
 	if angle_in_radians ~= 0 then
-		return 1 / math.sin(angle_in_radians)
+		return 1.0 / math.sin(angle_in_radians)
 	else
 		error("Attempted to perform sin(0), which is undefined!")
 	end
@@ -26,7 +26,7 @@ end
 ---@param angle_in_radians number --An angle in radians.
 ---@return number --The secant of angle_in_radians in radians.
 function trigreciprocal.sec(angle_in_radians)
-	return 1 / math.cos(angle_in_radians)
+	return 1.0 / math.cos(angle_in_radians)
 end
 
 ---The reciprocal of tan.
@@ -34,10 +34,36 @@ end
 ---@return number --The cotangent of angle_in_radians in radians.
 function trigreciprocal.cot(angle_in_radians)
 	if angle_in_radians ~= 0 then
-		return 1 / math.tan(angle_in_radians)
+		return 1.0 / math.tan(angle_in_radians)
 	else
 		error("Attempted to perform cot(0), which is undefined!")
 	end
+end
+
+---The inverse of csc.
+---@param angle_in_radians number --An angle in radians.
+---@return number --The arccosecant of angle_in_radians in radians.
+function trigreciprocal.acsc(angle_in_radians)
+	if angle_in_radians ~= 0 then
+		return math.asin(1.0 / angle_in_radians)
+	else
+		error("Attempted to perform arccsc(0), which is undefined!")
+	end
+end
+
+---The inverse of sec.
+---@param angle_in_radians number --An anglie in radians.
+---@return number --The arcsecant of angle_in_radians in radians.
+function trigreciprocal.asec(angle_in_radians)
+	if angle_in_radians ~= 0 then
+		return math.acos(1.0 / angle_in_radians)
+	else
+		error("Attempted to perform arcsec(0), which is undefined!")
+	end
+end
+
+function trigreciprocal.acot(angle_in_radians)
+	return math.atan(1.0 / angle_in_radians)
 end
 
 return trigreciprocal
