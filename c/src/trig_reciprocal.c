09163d9a2a099fa33b2a022e8468859cbab48093
Synthird <105394020+Synthird@users.noreply.github.com>
2025-05-05 14:16:14 -0700

    Shortened the copyright tag text in the C module

--- c/src/trig_reciprocal.c
+++ c/src/trig_reciprocal.c
@@ -2,7 +2,7 @@
  * @brief Trigonometric reciprocal identity methods that return radians.
  * @file trig_reciprocal.c
  * @author Synthird
- * @copyright MIT license
+ * @copyright MIT
  * @version 1.0.0
  */
 
