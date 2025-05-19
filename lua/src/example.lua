c2b69343446908c922866653d18d638aa1a6d55b
Synthird <105394020+Synthird@users.noreply.github.com>
2025-05-18 17:47:13 -0700

    Added inverse hyperbolic functions to the Lua module

--- lua/src/example.lua
+++ lua/src/example.lua
@@ -5,18 +5,26 @@ local trigreciprocal = require("trigreciprocal")
 print(string.format("csc(1) = %s", trigreciprocal.csc(1)))
 print(string.format("sec(1) = %s", trigreciprocal.sec(1)))
 print(string.format("cot(1) = %s", trigreciprocal.cot(1)))
+
 print("----")
 
 print(string.format("arccsc(1) = %s", trigreciprocal.acsc(1)))
 print(string.format("arcsec(1) = %s", trigreciprocal.asec(1)))
 print(string.format("arccot(1) = %s", trigreciprocal.acot(1)))
+
 print("----")
 
 print(string.format("csch(1) = %s", trigreciprocal.csch(1)))
 print(string.format("sech(1) = %s", trigreciprocal.sech(1)))
 print(string.format("coth(1) = %s", trigreciprocal.coth(1)))
 
-print("--- 0 as the parameter ---")
+print("----")
+
+print(string.format("arccsch(1) = %s", trigreciprocal.acsch(2)))
+print(string.format("arcsech(1) = %s", trigreciprocal.asech(1)))
+print(string.format("arccoth(1.1) = %s", trigreciprocal.acoth(1.1)))
+
+print("--- Doing pcalls ---")
 
 local cosecant_zero = pcall(trigreciprocal.csc, trigreciprocal, 0)
 local cotangent_zero = pcall(trigreciprocal.cot, trigreciprocal, 0)
@@ -26,6 +34,10 @@ local arcsecant_zero = pcall(trigreciprocal.asec, trigreciprocal, 0)
 
 local csch_zero = pcall(trigreciprocal.csch, trigreciprocal, 0)
 local coth_zero = pcall(trigreciprocal.coth, trigreciprocal, 0)
+
+local acsch_zero = pcall(trigreciprocal.acsch, trigreciprocal, 0)
+local asech_undefined = pcall(trigreciprocal.asech, trigreciprocal, 2)
+local acoth_undefined = pcall(trigreciprocal.acoth, trigreciprocal, 0.35)
 
 if type(cosecant_zero) ~= "number" then
 	print("csc(0) = undefined!")
@@ -59,4 +71,18 @@ print(string.format("sech(0) = %s", trigreciprocal.sech(0)))
 
 if type(coth_zero) ~= "number" then
 	print("coth(0) = undefined!")
+end
+
+print("----")
+
+if type(acsch_zero) ~= "number" then
+	print("arccsch(0) = undefined!")
+end
+
+if type(asech_undefined) ~= "number" then
+	print("arcsech(2) = undefined!")
+end
+
+if type(acoth_undefined) ~= "number" then
+	print("arccoth(0.35) = undefined!")
 end
--- lua/src/trigreciprocal.lua
+++ lua/src/trigreciprocal.lua
@@ -93,4 +93,38 @@ function trigreciprocal.coth(angle_in_radians)
 	end
 end
 
+---The inverse of csch.
+---@param angle_in_radians number --An angle in radians.
+---@return number --The hyperbolic arccosecant of angleInRadians in radians.
+function trigreciprocal.acsch(angle_in_radians)
+	if angle_in_radians ~= 0 then
+		return math.log(math.sqrt((1.0 / (angle_in_radians * angle_in_radians)) + 1.0) + (1.0 / angle_in_radians));
+	else
+		error("Attempted to perform arccsch(0), which is undefined!")
+	end
+end
+
+---The inverse of sech.
+---@param angle_in_radians number --An angle in radians.
+---@return number --The hyperbolic arcsecant of angleInRadians in radians.
+function trigreciprocal.asech(angle_in_radians)
+	if angle_in_radians <= 0 or angle_in_radians > 1 then
+		error("Attempted to perform arcsech on a number that is between 0 and 1, which is undefined!")
+	else
+		return math.log(math.sqrt((1.0 / angle_in_radians) - 1) * math.sqrt((1.0 / angle_in_radians) + 1) +
+		(1.0 / angle_in_radians));
+	end
+end
+
+---The inverse of coth.
+---@param angle_in_radians number --An angle in radians.
+---@return number --The hyperbolic arccotangent of angleInRadians in radians.
+function trigreciprocal.acoth(angle_in_radians)
+	if angle_in_radians < -1 or angle_in_radians > 1 then
+		return 0.5 * (math.log(angle_in_radians + 1) - math.log(angle_in_radians - 1))
+	else
+		error("Attempted to perform arccoth on a number that is less than -1 or greater than 1, which is undefined!")
+	end
+end
+
 return trigreciprocal
